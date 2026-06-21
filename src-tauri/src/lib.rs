b4aba87e3dbfcec7a73f02c9cfe9f1b5ef108b03
Adam Burness-Smith <adam@b-smith.me>
2026-06-21 21:56:22 +0100

    chore(tauri): register fetch_and_extract_pdf_url command in invoke handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -14,7 +14,11 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init())
-        .invoke_handler(tauri::generate_handler![greet, commands::file::extract_pdf_text])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            commands::file::extract_pdf_text,
+            commands::file::fetch_and_extract_pdf_url
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
